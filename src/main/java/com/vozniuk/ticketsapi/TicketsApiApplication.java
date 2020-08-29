62c967639abf68bf64b2d3ec187deb3ee51ba812
V0K0 <Kostya.V2000@gmail.com>
2020-08-29 13:52:06 +0300

    Configured application.

--- src/main/java/com/vozniuk/ticketsapi/TicketsApiApplication.java
+++ src/main/java/com/vozniuk/ticketsapi/TicketsApiApplication.java
@@ -2,12 +2,16 @@ package com.vozniuk.ticketsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@EnableScheduling
 public class TicketsApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketsApiApplication.class, args);
+       SpringApplication.run(TicketsApiApplication.class, args);
     }
 
 }
